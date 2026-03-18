1982b108ab19c5740e8757bc8e8612f8b96c0be3
unknown <henrytranwork2@gmail.com>
2026-03-18 18:47:46 +1100

    dsfdfd

---
+++ lib/utils.tsx
@@ -0,0 +1,22 @@
+export function toTitleCase(str: string) {
+  const smallWords = ["and", "or", "the", "of", "in", "on", "at", "to", "for"]
+
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word, index) => {
+
+      // Keep acronyms uppercase (CNC, API, etc)
+      if (word.length <= 3 && word === word.toUpperCase()) {
+        return word
+      }
+
+      // Keep small words lowercase unless first word
+      if (index !== 0 && smallWords.includes(word)) {
+        return word
+      }
+
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    .join(" ")
+}
