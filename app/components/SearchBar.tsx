32109919654a628fc487f5516401f0e3e209faaf
unknown <henrytranwork2@gmail.com>
2026-03-19 15:15:50 +1100

    sdsd

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import SupplierLogo from "@/app/components/SupplierLogo"
+import SupplierLogo from "./SupplierLogo"
 
 /* ---------------- TYPES ---------------- */
 
